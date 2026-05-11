0de60bad482f92ab9d286316d8091f6f02a6a051
UzielTzab19 <uzieltzab8@gmail.com>
2026-05-10 19:14:27 -0600

    feat: replace default pagination class with custom StandardResultsSetPagination for improved API response handling

--- core/settings.py
+++ core/settings.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'utils.pagination.StandardResultsSetPagination',
     'PAGE_SIZE': 50,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
